caba6a396750b17d3efc644bab757586ea5d07e8
Jareth <jarethronquillo12@gmail.com>
2026-09-22 15:35:59 +0800

    update project

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,29 +1,28 @@
 <template>
   <!-- Project X -->
-        
-    <div class="project-card">
-      <div class="project-thumb">
-        <img :src="project.image" alt="E-commerce App">
-      </div>
-      <div class="project-body">
-        <span class="project-tag">{{ project.tag }}</span>
-        <h5>{{ project.title }}</h5>
-        <p>{{ project.description }}</p>
-        <div>
-          <a :href="project.liveDemo" class="project-link" target="_blank" rel="nonopener nonreferrer">Live Demo</a>
-          <a :href="project.github" class="project-link" target="_blank" >GitHub</a>
-        </div>
+
+  <div class="project-card">
+    <div class="project-thumb">
+      <img :src="project.image" alt="E-commerce App">
+    </div>
+    <div class="project-body">
+      <span class="project-tag">{{ project.tag }}</span>
+      <h5>{{ project.title }}</h5>
+      <p>{{ project.description }}</p>
+      <div>
+        <a :href="project.liveDemo" class="project-link" target="_blank" rel="nonopener nonreferrer">Live Demo</a>
+        <a :href="project.github" class="project-link" target="_blank">GitHub</a>
       </div>
     </div>
-        
+  </div>
 </template>
 
 <script setup>
-  import { defineProps } from 'vue';
+import { defineProps } from 'vue';
 
-  defineProps({
-    project: Object,
-    required: true
-  });
+defineProps({
+  project: Object,
+  required: true
+});
 
 </script>
